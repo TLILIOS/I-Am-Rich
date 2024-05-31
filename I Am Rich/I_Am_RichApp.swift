2e81ded7e0b21e6be871ce6e57836360ed291cbe
TLiLi HAMDI <tlilios@yahoo.com>
2024-05-31 11:16:36 +0200

    Starting-SwiftUI

---
+++ I Am Rich/I_Am_RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_RichApp.swift
+//  I Am Rich
+//
+//  Created by HTLILI on 31/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
